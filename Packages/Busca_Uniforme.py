6bf167a9ee0566b037eb294024fd046c961c35d4
thoffart <thiagohoffart321920@gmail.com>
2018-05-06 18:04:11 -0300

    busca uniforme finalizada

--- Packages/Busca_Uniforme.py
+++ Packages/Busca_Uniforme.py
@@ -1,3 +1,29 @@
-class metodo(self):
+class metodo():
 
-    def __init__(self):
+    def __init__(self, fx, delta, a, b):
+        self.fx = fx
+        self.delta = delta
+        self.a = a
+        self.b = b
+
+    def calculaux(self, x, aux, result1, result2, deltaux):
+        while(result2 < result1 and aux < self.b):
+            result1 = result2
+            self.a = aux
+            if(aux + self.delta < self.b):
+                aux = aux + self.delta
+            else:
+                aux = self.b
+            x = aux
+            result2 = eval(self.fx)
+      #  if (aux != self.b && self.delta > deltaux/100):
+       #     calculaux(x,aux,re)
+
+    def calcula(self):
+        x = self.a
+        deltaux = self.delta
+        aux = self.a + self.delta
+        result1 = eval(self.fx)
+        x = aux
+        result2 = eval(self.fx)
+        self.calculaux(x, aux, result1, result2, deltaux)
--- main_program.py
+++ main_program.py
@@ -1,1 +1,4 @@
 from Packages import Busca_Uniforme
+
+Bu = Busca_Uniforme.metodo('x**2 + 2*x', 0.5, -3, 5)
+Bu.calcula()
