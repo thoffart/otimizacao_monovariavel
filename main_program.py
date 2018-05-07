a788c4d14acaf7d853896368e371f59cead36d3f
thoffart <thiagohoffart321920@gmail.com>
2018-05-07 09:11:23 -0300

    .

--- main_program.py
+++ main_program.py
@@ -1,8 +1,8 @@
 from Packages import Busca_Uniforme
 from Packages import Busca_Dicotomica
 
-#Bu = Busca_Uniforme.metodo('x**2 + 2*x', 0.5, -3, 5)
-# Bu.calcula()
+Bu = Busca_Uniforme.metodo('x**2 + 2*x', 0.5, -3, 5)
+Bu.calcula()
 
 bd = Busca_Dicotomica.metodo('x**2 + 2*x', 0.01, -3, 5, 0.1)
 bd.calcula()
